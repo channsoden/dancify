7bfe53025558bb9acd0296783d253d3cc51d3322
Christopher Hann-Soden <channsoden@gmail.com>
2019-03-10 13:23:58 -0700

    finish pagination

--- dancify/collections.py
+++ dancify/collections.py
@@ -43,30 +43,8 @@ def navpoints(page, pages):
 @bp.route('/library/<int:page>')
 @spotify_auth.login_required
 def library(page):
-    # Eventually should set these preferences and retrieve from g
-    sort_key = None
-    page_size = 100
-    
     track_dispenser = g.sp.current_user_saved_tracks()
-    tracks = sort_tracks(track_dispenser, sort_key=sort_key)
-
-    pages = ceil(len(tracks) / page_size)
-    page = max(1, min(page, pages))
-    page_navigations = set([1, page-10, page-5, page-4, page-3, page-2, page-1, page+1, page+2, page+3, page+4, page+5, page+10, pages])
-    page_urls = [url_for('collections.library', page=i) for i in 
-    track_selection = slice( (page-1)*100, page*100 )
-    
-    tracks = spotipy_fns.get_track_info(track_dispenser)
-
-    columns = g.preferences['collections']['columns']
-
-    class playlist_table(Table):
-        for iname, xname in track_features.items():
-            locals()[iname] = Col(xname, show = iname in columns)
-
-    table = playlist_table(tracks)
-
-    return render_template('/collections/collection.html', collection='Saved Tracks', table=table.__html__())
+    return collection(track_dispenser, page, 'Saved Tracks')
     
 @bp.route('/playlists')
 @spotify_auth.login_required
@@ -81,12 +59,31 @@ def playlists():
     return render_template('collections/playlists.html', lists=lists)
 
 
-@bp.route('/playlists/<uid>/<plid>')
+@bp.route('/playlists/<uid>/<plid>/<int:page>')
 @spotify_auth.login_required
-def playlist(uid, plid):
+def playlist(uid, plid, page):
+    pl = g.sp.user_playlist(uid, playlist_id=plid)
+    print(pl.keys())
+    
     track_dispenser = g.sp.user_playlist_tracks(uid, playlist_id=plid)
-    tracks = spotipy_fns.get_track_info(track_dispenser)
-        
+    return collection(track_dispenser, page, 'get playlist name')
+
+def collection(track_dispenser, page, collection_name):
+    # Eventually should set these preferences and retrieve from g
+    sort_key = None
+    page_size = 100
+    
+    tracks = spotipy_fns.sort_tracks(track_dispenser, sort_key=sort_key)
+
+    pages = ceil(len(tracks) / page_size)
+    page = max(1, min(page, pages))
+    pagination = navpoints(page, pages)
+    page_urls = [(str(i), url_for('collections.library', page=i)) for i in pagination]
+    track_selection = slice( (page-1)*100, page*100 )
+
+    tracks = tracks[track_selection]
+    tracks = spotipy_fns.get_track_info(tracks)
+
     columns = g.preferences['collections']['columns']
 
     class playlist_table(Table):
@@ -94,6 +91,9 @@ def playlist(uid, plid):
             locals()[iname] = Col(xname, show = iname in columns)
 
     table = playlist_table(tracks)
-        
-    return render_template('/collections/collection.html', collection='collection name', table=table.__html__())
+
+    return render_template('/collections/collection.html',
+                           collection=collection_name,
+                           table=table.__html__(),
+                           page_links = page_urls)
 
